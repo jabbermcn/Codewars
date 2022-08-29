fb59995af0e0ea399c778e443781a75836b3c376
mikhailouski_n <jabber_mcn@tut.by>
2022-08-29 08:56:14 +0300

    add some codewars tasks

--- solution(римские цифры).py
+++ solution(римские цифры).py
@@ -1,3 +1,6 @@
+"""Переводим арабские числа в римские"""
+
+
 def solution(n: int) -> str:
     ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
     tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
@@ -8,3 +11,16 @@ def solution(n: int) -> str:
 
 a = solution(2020)
 print(a, type(a))
+
+"""Парсим римские числа, на ввод приходит римский символ"""
+romans = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
+
+
+def parse_roman(roman):
+    result = 0
+    for i, c in enumerate(roman):
+        if i + 1 < len(roman) and romans[i + 1] < romans[roman[i + 1]]:
+            result -= roman[i + 1]
+        else:
+            result += roman[i + 1]
+    return result
